b0dc50b684f70fbe3c256dabf40254b947438a30
gausvanya <gaus.vanya_2006@mail.ru>
2024-09-26 20:41:42 +0300

    #new add new module / middleware filter spam/raid message

--- app/bot/Filters/__init__.py
+++ app/bot/Filters/__init__.py
@@ -2,3 +2,4 @@ from .Command import Command
 from .IsAdminFilter import IsAdminFilter
 from .GetUser import GetUserInfo
 from .IsAgentFilter import IsAgentFilter
+from .MessageFilter import MessageFilter
